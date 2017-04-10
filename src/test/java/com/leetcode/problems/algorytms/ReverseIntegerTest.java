58707999ea2f5d7d34cb6aae988ef714e9b6a4e1
vvv1559 <vvv1559@yandex.com>
2017-04-10 11:11:21 +0300

    Reverse Integer solution

---
+++ src/test/java/com/leetcode/problems/algorytms/ReverseIntegerTest.java
@@ -0,0 +1,32 @@
+package com.leetcode.problems.algorytms;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ReverseIntegerTest {
+    private final ReverseInteger reverseInteger = new ReverseInteger();
+
+    @Test
+    public void testExamples() {
+        Assert.assertEquals(321, reverseInteger.reverse(123));
+        Assert.assertEquals(-321, reverseInteger.reverse(-123));
+        Assert.assertEquals(-1, reverseInteger.reverse(-10));
+        Assert.assertEquals(-101, reverseInteger.reverse(-101));
+    }
+
+
+    @Test
+    public void testNotes() {
+        Assert.assertEquals(1, reverseInteger.reverse(10));
+        Assert.assertEquals(1, reverseInteger.reverse(100));
+        Assert.assertEquals(-1, reverseInteger.reverse(-100));
+    }
+
+    @Test
+    public void testOverflow() {
+        Assert.assertEquals(0, reverseInteger.reverse(1000000003));
+        Assert.assertEquals(0, reverseInteger.reverse(-1000000003));
+        Assert.assertEquals(0, reverseInteger.reverse(1534236469));
+        Assert.assertEquals(0, reverseInteger.reverse(0));
+    }
+}
