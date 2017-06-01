166d167902acf0c8e0e39906ba8174e64b2e9226
vvv1559 <vvv1559@yandex.com>
2017-06-01 10:35:51 +0300

    Valid Parentheses

---
+++ src/main/java/com/leetcode/problems/algorytms/ValidParentheses.java
@@ -0,0 +1,45 @@
+package com.leetcode.problems.algorytms;
+
+import com.Difficulty;
+import com.SolutionComplexity;
+
+import java.util.Stack;
+
+import static com.Difficulty.Level.EASY;
+
+/*
+* Original text: https://leetcode.com/problems/valid-parentheses/#/description
+*
+* Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+*
+* The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+*
+* */
+
+@Difficulty(EASY)
+class ValidParentheses {
+    @SolutionComplexity(complexity = "O(n)", extraMemory = "O(n)")
+    boolean isValid(String s) {
+        final Stack<Character> braces = new Stack<>();
+
+        for (char brace : s.toCharArray()) {
+            final boolean isCloseBrace = brace == ')' || brace == '}' || brace == ']';
+
+            if (isCloseBrace) {
+                if (braces.empty()) {
+                    return false;
+                }
+
+                final char prevBrace = braces.pop();
+                if ((brace == ')' && prevBrace != '(')
+                    || (brace == '}' && prevBrace != '{')
+                    || (brace == ']' && prevBrace != '[')) {
+                    return false;
+                }
+            } else {
+                braces.push(brace);
+            }
+        }
+        return braces.empty();
+    }
+}
---
+++ src/test/java/com/leetcode/problems/algorytms/ValidParenthesesTest.java
@@ -0,0 +1,22 @@
+package com.leetcode.problems.algorytms;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ValidParenthesesTest {
+
+    private final ValidParentheses validParentheses = new ValidParentheses();
+
+    @Test
+    public void fromExamples() throws Exception {
+        Assert.assertTrue(validParentheses.isValid("()"));
+        Assert.assertTrue(validParentheses.isValid("()[]{}"));
+        Assert.assertFalse(validParentheses.isValid("(]"));
+        Assert.assertFalse(validParentheses.isValid("([)]"));
+    }
+
+    @Test
+    public void fromTests() throws Exception {
+        Assert.assertFalse(validParentheses.isValid("("));
+    }
+}
