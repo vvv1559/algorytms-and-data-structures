fe605dc5842ab11b4ba88b95795e96eda5b3fcbf
vvv1559 <vvv1559@yandex.com>
2017-10-24 10:13:12 +0300

    Is palindrome solution

---
+++ src/main/java/com/leetcode/problems/algorytms/easy/IsPalindrome.java
@@ -0,0 +1,47 @@
+package com.leetcode.problems.algorytms.easy;
+
+import com.Difficulty;
+import com.SolutionComplexity;
+
+import static com.Difficulty.Level.EASY;
+
+/*
+* Original text: https://leetcode.com/problems/valid-palindrome/description/
+*
+* Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+* For example,
+*
+* "A man, a plan, a canal: Panama" is a palindrome.
+* "race a car" is not a palindrome.
+*
+* Note:
+* Have you consider that the string might be empty? This is a good question to ask during an interview.
+* For the purpose of this problem, we define empty string as valid palindrome.
+* */
+
+@Difficulty(EASY)
+class IsPalindrome {
+
+    @SolutionComplexity(complexity = "O(n)", extraMemory = "O(1)")
+    boolean isPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        while (start < end) {
+            char startChar = Character.toLowerCase(s.charAt(start));
+            char endChar = Character.toLowerCase(s.charAt(end));
+
+            if (!Character.isLetter(startChar) && !Character.isDigit(startChar)) {
+                start++;
+            } else if (!Character.isLetter(endChar) && !Character.isDigit(endChar)) {
+                end--;
+            } else if (startChar != endChar) {
+                return false;
+            } else {
+                start++;
+                end--;
+            }
+        }
+
+        return true;
+    }
+}
